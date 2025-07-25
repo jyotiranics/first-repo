25cee95437133b37751a5a511da3989d24f527b4
jyotiranics <jyotisahoo.2024@gmail.com>
2025-07-25 20:19:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let variabl1= "Gayatri", variabl2 = "Nayak"
+variabl1 = "something"
+
+let x1;
+x1 = 105;
+console.log(variabl1, x1);
+const x = 45, y =27, z = 90;
+console.log(x+y+z);
+
+
+console.log(a);
+
+var a = 590, b = 200;
+var a = 900
+var a;
+a = 55
+console.log(b-a);
+
+let htmlForm = document.getElementById("form");
+let inputUserName = document.createElement("input")
+inputUserName.placeholder = "Enter your name"
+
+inputUserName.classList.add("input")
+inputUserName.classList.add("input")
